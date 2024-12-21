1ed230a32d51b1ed1e644015bd575436dc81ce34
Scott <451549861@qq.com>
2024-12-21 19:28:43 +0800

    update kruisegamectl examples

--- examples/kruisegamectl/kruisegamectl.sh
+++ examples/kruisegamectl/kruisegamectl.sh
@@ -3,13 +3,13 @@
 kube_config_path=/root/.kube/config
 
 # update the updatePriority of all the GS to 1
-kruisegamectl --kubeconfig="${kube_config_path}" --pretty=true --jsonpatch="[{\"op\":\"replace\",\"path\":\"/spec/updatePriority\",\"value\":1}]"
+kruisegamectl -k "${kube_config_path}" -r gameserver -p -j "[{\"op\":\"replace\",\"path\":\"/spec/updatePriority\",\"value\":1}]"
 
 # update the updatePriority of GS with opsState Allocated to 2
-kruisegamectl --kubeconfig="${kube_config_path}" --pretty=true --filter='{"opsState":"Allocated"}' --jsonpatch='[{"op":"replace","path":"/spec/updatePriority","value":2}]'
+kruisegamectl -k "${kube_config_path}" -r gameserver -p -f '{"opsState":"Allocated"}' -j '[{"op":"replace","path":"/spec/updatePriority","value":2}]'
 
 # update the updatePriority of GS with opsState None to 3
-kruisegamectl --kubeconfig="${kube_config_path}" --pretty=true --filter="{\"opsState\":\"None\"}" --jsonpatch="[{\"op\":\"replace\",\"path\":\"/spec/updatePriority\",\"value\":3}]"
+kruisegamectl -k "${kube_config_path}" -r gameserver -p -f "{\"opsState\":\"None\"}" -j "[{\"op\":\"replace\",\"path\":\"/spec/updatePriority\",\"value\":3}]"
 
 # get all the GS with updatePriority 2
-kruisegamectl --kubeconfig="${kube_config_path}" --pretty=true --filter="{\"updatePriority\":2}"
+kruisegamectl -k "${kube_config_path}" -r gameserver -p -f "{\"updatePriority\":2}"
